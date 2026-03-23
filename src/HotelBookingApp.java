43edda7b858fe8520d3b30b21cf48eaf8a82e9be
Avinav Kumar <avinavkumar0045@gmail.com>
2026-03-23 08:35:21 +0530

    initliazing Book my app

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,13 @@
+// UC1: Application Entry & Welcome Message
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("=================================");
+
+    }
+}
